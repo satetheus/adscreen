8d1019e1908a33339dd365af952360e1098ad20a
Satetheus <chriswarrenmccullough@gmail.com>
2019-04-11 15:08:52 -0600

    Fixed timers for picture switching

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 inc1 = 0;
 inc2 = 0;
-inc3 = 0;
+inc3 = 3;
+switcher = 0;
 
 images = ["img/large_ad/image1.jpg",
           "img/large_ad/image2.jpg",
@@ -19,7 +20,6 @@ small_ads = ["img/small_ad/image1.png",
 function changeImage(ad_id, list_id, increment) {
     var img = document.getElementById(ad_id);
     img.src = list_id[increment];
-    increment++;
     if(increment >= list_id.length){increment = 0;}
     fadeImg(img, 100, false);
 }
@@ -35,6 +35,23 @@ function fadeImg(el, val, fade){
 }
 
 // this doesn't work, see: https://johnresig.com/blog/how-javascript-timers-work/
-setInterval(function(){changeImage("big_picture", images, inc1)}, 13000);
-setInterval(function(){changeImage("small_top_ad", small_ads, inc2)}, 7000);
-setInterval(function(){changeImage("small_bottom_ad", small_ads, inc3)}, 9000);
+setInterval(function() {
+  if (switcher == 0) {
+    inc1++;
+    if(inc1>=images.length){inc1=0;}
+    changeImage("big_picture", images, inc1);
+    switcher++;
+  }
+  else if (switcher == 1) {
+    inc2++;
+    if(inc2>=small_ads.length){inc2=0;}
+    changeImage("small_top_ad", small_ads, inc2);
+    switcher++;
+  }
+  else if (switcher >= 2){
+    inc3++;
+    if(inc3>=small_ads.length){inc3=0;}
+    changeImage("small_bottom_ad", small_ads, inc3);
+    switcher = 0;
+  }
+}, 1000);

d4455cf5e3e2b3306083c4f87cd5867a633ac3ef
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-04-30 12:40:14 -0600

    successful image upload to img/large_ad

--- adminSite/upload.php
+++ adminSite/upload.php
@@ -1,12 +1,12 @@
 <?php
 // starting code from https://www.w3schools.com/php/php_file_upload.asp
 
-$target_dir = "../displaySite/img/";
+$target_dir = "../displaySite/img/large_ad/";
 
 $big_image = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 
 // if everything is ok, try to upload file
-if (imageCheck($big_image) == True) {
+if (imageCheck($big_image, "fileToUpload") == True) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $big_image)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     }
@@ -17,10 +17,10 @@ if (imageCheck($big_image) == True) {
 }
 
 
-function imageCheck($target_file) {
+function imageCheck($target_file, $file_name) {
     // Check if image file is a actual image or fake image
     if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]); //To-Do: Update fileToUpload to a variable?
+        $check = getimagesize($_FILES[$file_name]["tmp_name"]); //To-Do: Update fileToUpload to a variable?
         if($check == false) {
             echo "File is not an image.";
             return False;
@@ -34,7 +34,7 @@ function imageCheck($target_file) {
     }
     
     // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 500000) {
+    if ($_FILES[$file_name]["size"] > 500000) {
         echo "Sorry, your file is too large.";
         return False;
     }

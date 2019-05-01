5b196b8c12ae51b5b8a079c9b35f996304737734
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-04-30 20:01:17 -0600

    setup for small_ad upload

--- adminSite/upload.php
+++ adminSite/upload.php
@@ -1,14 +1,16 @@
 <?php
 // starting code from https://www.w3schools.com/php/php_file_upload.asp
 
-$target_dir = "../displaySite/img/large_ad/";
+$big_dir = "../displaySite/img/large_ad/";
+$big_image = $big_dir . basename($_FILES["bigImage"]["name"]);
 
-$big_image = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$small_dir = "../displaySite/img/small_ad";
+$small_image = $small_dir . basename($_FILES["smallImage"]["name"]);
 
 // if everything is ok, try to upload file
-if (imageCheck($big_image, "fileToUpload") == True) {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $big_image)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+if (imageCheck($big_image, "bigImage") == True) {
+    if (move_uploaded_file($_FILES["bigImage"]["tmp_name"], $big_image)) {
+        echo "The file ". basename( $_FILES["bigImage"]["name"]). " has been uploaded.";
     }
     
     else {
@@ -20,7 +22,7 @@ if (imageCheck($big_image, "fileToUpload") == True) {
 function imageCheck($target_file, $file_name) {
     // Check if image file is a actual image or fake image
     if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES[$file_name]["tmp_name"]); //To-Do: Update fileToUpload to a variable?
+        $check = getimagesize($_FILES[$file_name]["tmp_name"]); //To-Do: Update bigImage to a variable?
         if($check == false) {
             echo "File is not an image.";
             return False;

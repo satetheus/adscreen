005ff5408a6fdfd7068487f8eca16c6402c33d81
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-05-05 21:21:41 -0600

    created feedback on edit submission

--- adminSite/edit.php
+++ adminSite/edit.php
@@ -1,6 +1,7 @@
 <?php
 include "../displaySite/import.php";
 
+
 echo '<!DOCTYPE html>
 <html>
 <body>';
@@ -8,18 +9,35 @@ echo '<!DOCTYPE html>
 
 if(isset($_GET['size'])) {
     $dir = '../displaySite/img/'.$_GET['size'].'_ad/';
-    $images = importImages($dir);
+    $image = importImages($dir)[$_GET['index']];
+    $name = str_replace($dir, "", $image);
+}
+
+else {
+    // Redirect if parameters are not set
+    header("location: view.php");
+    exit;
 }
 
 echo '<form action="" method="post" enctype="multipart/form-data">
 Rename Image
-<input type="text" name="FileName" value="New Name" id="newName">
+<input type="text" name="fileName" value="'.$name.'" id="newName">
 <input type="submit" value="Rename" name="editSubmit">
-</form> 
-  
-<span><p>Delete</p></span>';
+</form>'; 
 
-echo "<img src='".$images[$_GET['index']]."'/>";
+if(isset($_POST['editSubmit'])) {
+    // Check if file already exists
+    if (file_exists($dir.$_POST['fileName'])) {
+        echo "Sorry, file already exists.";
+    }
+    else {
+        echo 'Success!';
+    }
+}
+
+echo '<span><p>Delete</p></span>';
+
+echo "<img src='".$image."'/>";
 
 echo '
 </body>

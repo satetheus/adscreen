77f0910db4ac622a8648da8cfd279021c9a9d792
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-05-05 20:31:09 -0600

    setup edit form in adminSite

--- adminSite/edit.php
+++ adminSite/edit.php
@@ -1,10 +1,30 @@
 <?php
+include "../displaySite/import.php";
 
 echo '<!DOCTYPE html>
 <html>
 <body>';
 
-echo $_GET['index'];
+
+if(isset($_GET['size'])) {
+    if($_GET['size'] == 'large') {
+        $images = importImages('../displaySite/img/large_ad/');
+    }
+
+    else if($_GET['size'] == 'small') {
+        $images = importImages('../displaySite/img/small_ad/');
+    }
+}
+
+echo '<form action="" method="post" enctype="multipart/form-data">
+Rename Image
+<input type="text" name="FileName" value="New Name" id="newName">
+<input type="submit" value="Rename" name="editSubmit">
+</form> 
+  
+<span><p>Delete</p></span>';
+
+echo "<img src='".$images[$_GET['index']]."'/>";
 
 echo '
 </body>

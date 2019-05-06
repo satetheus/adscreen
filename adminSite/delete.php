5577c9129d926ceddf9a138c382e348fafe2b1bd
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-05-06 10:12:25 -0600

    delete functionality added

--- adminSite/delete.php
+++ adminSite/delete.php
@@ -6,6 +6,30 @@ echo '<!DOCTYPE html>
 <html>
 <body>';
 
+if(isset($_GET['size'])) {
+    $dir = '../displaySite/img/'.$_GET['size'].'_ad/';
+    $image = importImages($dir)[$_GET['index']];
+    $name = str_replace($dir, "", $image);
+}
+
+else {
+    // Redirect if parameters are not set
+    header("location: view.php");
+    exit;
+}
+
+echo '
+<form action="" method="post" enctype="multipart/form-data">
+    Are you sure you want to delete '.$name.'?
+    <input type="submit" value="Delete Image" name="deleteImage">
+</form>';
+
+if(isset($_POST['deleteImage'])) {
+    unlink($image) || die('Couldn\'t delete image.');
+    if(!file_exists($image)) {
+        echo $name.' deleted.';
+    }
+}
 
 echo '
 </body>

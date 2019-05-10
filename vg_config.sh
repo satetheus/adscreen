789d287adb384cc9bf041599494c8884e096818e
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-05-10 14:52:05 -0600

    added composer & heroku to vg_config.sh

--- vg_config.sh
+++ vg_config.sh
@@ -1,2 +1,7 @@
 sudo apt-get update
 sudo apt-get install apache2 php libapache2-mod-php -y
+./composer_programmic_install.sh
+php composer.phar install
+sudo snap install heroku --classic
+heroku login
+heroku create
